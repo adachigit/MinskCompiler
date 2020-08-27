4ec713931e3981d4cbd3b60cadeb4302fc271b57
HanYi <hanyi@oasgames.com>
2020-08-27 20:33:16 +0800

    Add syntax tree.

--- MinskCompiler/Program.cs
+++ MinskCompiler/Program.cs
@@ -15,20 +15,20 @@ namespace MinskCompiler
                     return;
 
                 var parser = new Parser(line);
-                SyntaxNode node = parser.Parse();
+                SyntaxTree syntaxTree = parser.Parse();
 
                 var color = Console.ForegroundColor;
                 Console.ForegroundColor = ConsoleColor.DarkBlue;
 
-                PrettyPrint(node);
+                PrettyPrint(syntaxTree.Root);
 
                 Console.ForegroundColor = color;
 
-                if(parser.Diagnostics.Any())
+                if(syntaxTree.Diagnostics.Any())
                 {
                     Console.ForegroundColor = ConsoleColor.DarkRed;
 
-                    foreach(var diagnostic in parser.Diagnostics)
+                    foreach(var diagnostic in syntaxTree.Diagnostics)
                         Console.WriteLine(diagnostic);
 
                     Console.ForegroundColor = color;
@@ -232,6 +232,20 @@ namespace MinskCompiler
         }
     }
 
+    sealed class SyntaxTree
+    {
+        public SyntaxTree(IEnumerable<string> diagnostics, ExpressionSyntax root, SyntaxToken endOfFileToken)
+        {
+            Diagnostics = diagnostics.ToArray();
+            Root = root;
+            EndOfFileToken = endOfFileToken;
+        }
+
+        public IReadOnlyList<string> Diagnostics { get; }
+        public ExpressionSyntax Root { get; }
+        public SyntaxToken EndOfFileToken { get; }
+    }
+
     class Parser
     {
         private readonly SyntaxToken[] _tokens;
@@ -292,7 +306,15 @@ namespace MinskCompiler
             return new SyntaxToken(kind, Current.Position, null, null);
         }
 
-        public ExpressionSyntax Parse()
+        public SyntaxTree Parse()
+        {
+            var expression = ParseExpression();
+            var endOfFileToken = Match(SyntaxKind.EndOfFileToken);
+
+            return new SyntaxTree(_diagnostics, expression, endOfFileToken);
+        }
+
+        public ExpressionSyntax ParseExpression()
         {
             var left = ParsePrimaryExpression();
 
