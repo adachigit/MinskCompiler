28621d235ce930cce4a71921e76b2372bde0e2bb
HanYi <shuangbaotai@263.net>
2020-09-07 00:18:58 +0800

    bug fix

--- MinskCompiler/CodeAnalysis/Binding/BoundBinaryOperator.cs
+++ MinskCompiler/CodeAnalysis/Binding/BoundBinaryOperator.cs
@@ -21,14 +21,14 @@ namespace MinskCompiler.CodeAnalysis.Binding
             Kind = kind;
             LeftType = leftType;
             RightType = rightType;
-            ResultType = resultType;
+            Type = resultType;
         }
 
         public SyntaxKind SyntaxKind { get; }
         public BoundBinaryOperatorKind Kind { get; }
         public Type LeftType { get; }
         public Type RightType { get; }
-        public Type ResultType { get; }
+        public Type Type { get; }
 
         private static BoundBinaryOperator[] _operators = 
         {
